afad41dd07319c126ccd2067bb7d92f014e18ef5
omgzui <862275679@qq.com>
2017-12-06 15:43:47 +0800

    ++

--- src/Demo.php
+++ src/Demo.php
@@ -17,6 +17,9 @@ class Demo
         $this->items = $items;
     }
 
+    /**
+     * curl -XPOST -H'content-type:application/json' 'https://packagist.org/api/update-package?username=OMGZui&apiToken=AIWHAEhCxVfBvgPdltsM' -d'{"repository":{"url":"https://github.com/OMGZui/demo-sdk-for-Laravel5.5"}}'
+     */
     public function getName()
     {
         echo config('omgzui.name');
